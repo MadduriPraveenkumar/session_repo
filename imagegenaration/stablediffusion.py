588ee3e17ccb7b7db08737b45ce224efcdd301aa
praveenkumar-orchasp <t-praveenkumar.m@orchasp.com>
2025-11-26 17:48:18 +0530

    added image_generation function

---
+++ imagegenaration/stablediffusion.py
@@ -0,0 +1,50 @@
+import json
+import requests
+import logging
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+api_key = "key"
+
+logger = logging.getLogger(__name__)
+
+@api_view(["POST"])
+def image_generation(request):
+    logger.info("In image_generation")
+    try:
+        payload = request.data
+        prompt = payload.get("prompt")
+        aspect_ratio = payload.get("aspect_ratio")
+        output_format = payload.get("output_format")
+        
+        if not prompt or not aspect_ratio or not output_format:
+            logger.error("Missing required fields in the request payload")
+            return Response({"status": "error", "detail": "Missing required fields"})
+
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json"
+        }
+        
+        response = requests.post("https://api.stability.ai/v2beta/stable-image/generate/sd3", headers=headers, json=payload)
+        
+        if response.status_code == 200:
+            pass
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+ 
+    except json.DoesNotExist:
+        logger.error("Invalid JSON format for scenes")
+        return Response({"status": "error", "detail": "Invalid Json format"})
+    except Exception as e:
+        logger.exception("An unexpected error occured: %s", str(e))
+        return Response({"status": "error", "detail": "An unexcepted error occured"}, status=500)
+
+# payload = {
+#     "prompt": "a futuristic city at sunset",
+#     "aspect_ratio": "16:9",
+#     "output_format": "png"
+# }
+
+
