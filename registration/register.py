f5599db814b5810bd3cc27811c36465ba253f710
praveenkumar-orchasp <t-praveenkumar.m@orchasp.com>
2025-11-27 18:10:14 +0530

    modified register function

--- registration/register.py
+++ registration/register.py
@@ -1,2 +1,6 @@
-def user_registration():
+from .serializers import UserSerializer
+from rest_framework.decorators import api_view
+
+def user_registration(request):
+    serializer = UserSerializer(data=request.data)
     pass
